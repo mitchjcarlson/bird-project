ee0ca2c0cacaa0bed451c1674c839b9471aaf626
Jay Hennen <jahennen28@hotmail.com>
2012-10-12 20:03:31 -0700

    Okay, pushing from laptop this time, lets see if this shit I set up works

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("This is a little change");
 		System.out.println("Trying to push from eclipse");
 		System.out.println("Commit from eclipse works, but not sync to server yet");
+		System.out.println("Okay, commit/push from my laptop");
 	}
 }
