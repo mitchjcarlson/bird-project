ab2fc8723da9fb06b6f8e81ac956bd2dbaa02ad3
Jay Hennen <jahennen28@hotmail.com>
2012-10-12 19:55:04 -0700

    Commit from Eclipse? Fuck yeah

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is a little change");
+		System.out.println("Trying to push from eclipse");
 	}
 }
