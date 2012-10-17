0a9523391b69c23077ac00f1db5ac1c792d92206
Drew Harvey <drew.harvey.10@gmail.com>
2012-10-17 12:16:04 -0700

    Test #2

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		
-		// Drew - Successful push
+		// Drew - Successful push w/o commiting to local repo
 		
 		System.out.println("Hello Git!");
 		System.out.println("This is a little change");
