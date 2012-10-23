a4263a1ad28c55fbc078265cbad89853cea0f833
JHuisingh <JHuisingh@69.91.217.25>
2012-10-23 13:53:05 -0700

    More JUnit

--- BothellBirder/src/AllTests.java
+++ BothellBirder/src/AllTests.java
@@ -6,5 +6,4 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ TestFailure.class })
 public class AllTests {
-
 }
--- BothellBirder/src/TestFailure.java
+++ BothellBirder/src/TestFailure.java
@@ -8,5 +8,4 @@ public class TestFailure {
 	public void testFailure() throws Exception {
 	    fail();
 	}
-
 }
