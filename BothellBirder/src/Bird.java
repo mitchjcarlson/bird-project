6a0faafc81081226f0d4144fa77387c3f3f6238a
Jonathan Huisingh <jon.huisingh@gmail.com>
2012-11-18 13:40:18 -0800

    still just playing with Bird class, some questions in comments and REPORT 5 in the docs.
    Adding getting setters etc..

--- BothellBirder/src/Bird.java
+++ BothellBirder/src/Bird.java
@@ -1,11 +1,20 @@
 
+import 	java.util.Calendar;
+
+
+
 public class Bird {
+	enum GETSET{
+		SUCCESS,
+		FAILURE,
+		UNKNOWN_ERROR;}
+
 	private int birdID;
-	private String birdColors, birdLocation, birdDescription, birdPicture, birdSound, birdScientificName;
+	private String birdVideo, birdColors, birdLocation, birdDescription, birdPicture, birdSound, birdScientificName;
 	
 	//this is obviously not even close to done, just wanted to play with it a bit since it's been a year since I used java
 	public Bird(int idNum, String colors, String loc, String description, String pic, String sciName, String sound ) {
-	    birdID = idNum; //I'm guessing we actually want the birdclass to provide the num here, and return it? or do we want to manually set it each time?
+	    birdID = birdID(); //I'm guessing we actually want the birdclass to provide the num here, and return it? or do we want to manually set it each time?
 	    birdColors = colors;
 	    birdLocation = loc; //probably dont want this as a string, just getting something in here for now
 	    birdDescription = description;
@@ -17,5 +26,46 @@ public class Bird {
 	public Bird() {
 	    //default constructor, need to have conversations about this
 	}
-	//will get getter setters in here in a bit, after some lunch
+	
+	//for testing purposes
+	public static void main(String [] args)
+	{
+		birdID();
+	}
+	//just a thought, bird id could be time in milisecconds since this should never be repeated?
+	//or do we want a unique DB entry identifier num?
+	//Drew, can the DB assign the ID?
+	private static int birdID(){
+		Calendar now = Calendar.getInstance(); 
+		int time = (int)now.getTimeInMillis();
+	    System.out.println(now);
+	    System.out.println(time);
+	    return time;
+	}
+	
+	//getters and setters
+	public String getColor(){ return (birdColors == null)? "COLOR NOT SET!" : birdColors;}
+	public void setColor(String colorSet){ birdColors = colorSet;}
+	public String getLocation(){ return (birdLocation == null) ? "LOC NOT SET!" : birdLocation;}
+	public void setLocation(String locationSet){ birdLocation = locationSet;}
+	public String getPicture(){return (birdPicture == null) ? "PICTURE NOT SET!" : birdPicture;}
+	//We probably want a separate picture class that can format based on device
+	//size, unless android can do this on it's own?
+	public void setPicture(String pictureSet){ birdPicture = pictureSet;}
+	public String getVideo(){ return (birdVideo == null) ? "VIDEO NOT SET!" : birdVideo;}
+	public void setVideo(String videoSet){birdVideo = videoSet;}
+	public String getSound(){ return (birdSound == null) ? "SOUND NOT SET!" : birdSound;}
+	public void setSound(String sound){birdSound = sound;}
+	public String getDescription(){
+		
+	}
+	public String setDescription(){
+		
+	}
+	public String getScientificName(){
+		
+	}
+	public String setScientificName(){
+		
+	}
 }
