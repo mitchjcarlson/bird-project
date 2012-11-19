222d5de04406424095a47e259318f8b1ba151bca
Mitch Carlson <mitchjcarlson@gmail.com>
2012-11-19 02:22:45 -0800

    Added some comments

--- BothellBirder/src/Search.java
+++ BothellBirder/src/Search.java
@@ -3,11 +3,23 @@ public class Search {
 	
 	private String[] dbQueryValues;
 	
+	/**Constructor
+	 * Parses initial search string, splitting it into an array by non-word
+	 * characters followed by whitespace or just splitting it by whitespace.
+	 * @pre initialString is not null
+	 * @post initialized Search object with database query values in private
+	 * data member array
+	 * @param initialString:String
+	 */
 	public Search(String initialString) {
 		initialString = initialString.trim();
 		dbQueryValues = initialString.split("[(\\W++)(\\s++)](\\s++)");
 	}
 	
+	/**Get
+	 * @param index >= 0; int
+	 * @return database query value; String
+	 */
 	public String get(int index) {
 		return dbQueryValues[index];
 	}
