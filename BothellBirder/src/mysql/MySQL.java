f5d14110ba8e2005fc4922b915cda95e37e94845
Drew Harvey <drew.harvey.10@gmail.com>
2012-11-24 12:46:53 -0800

    Testing Database
    I am testing the database.  If anyone is trying to use it expect for
    vales to be added/removed/updated throughout the next hour

--- BothellBirder/src/mysql/MySQL.java
+++ BothellBirder/src/mysql/MySQL.java
@@ -37,7 +37,7 @@ public class MySQL {
 	
 	
 	
-	// Grabs information from database
+	// Grabs information from database 
 	
 	public ResultSet grabFromDatabase( String query ) throws Exception {
 		
